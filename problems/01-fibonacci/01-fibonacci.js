23a3bbc7f6306ca9ceb4b84ffad97ace196bd591
Chrischale Panditharathne <cpanda27@bu.edu>
2020-06-12 18:04:53 -0400

    chrish q1

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,22 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
+
 function fib(n) {
+    let fib = [0, 1];
+    if (n == 1 | n == 2){
+        console.log("nth fibonacci numner is: " + fib[n - 1]);
+        return;
+    }
+
+    for (let i = 2; i <= n; i++){
+        fib[i] = fib[i-1] + fib [i-2]
+    }
+
+    console.log("nth fibonacci number is: " + fib[n - 1]);
+    return;
 
 }
+
+fib(1);
+
