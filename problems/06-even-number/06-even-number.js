3e905f590430834a52878779f26f1ef396641fcc
Chrischale Panditharathne <cpanda27@bu.edu>
2020-06-15 11:30:57 -0400

    chrish q6

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,5 +3,15 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
-  
+    let i = 0;
+    let flag = true;
+    while(i<someNum){
+        flag = !flag;
+        i++;
+    }
+
+    return flag;
+
 }
+let j = 5;
+console.log("Is " + j + " even? " + isEven(j));
