14c65f4b4d3909f398120727bab9edba6beb4fb1
Chrischale Panditharathne <cpanda27@bu.edu>
2020-06-15 10:34:56 -0400

    chrish q3 - ?

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,15 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
+    let rev = [];
+    let len = someStr.length
+    for (i = 0; i <= someStr.length; i++){
+        rev[i] = (someStr.charAt(len-1));
+        len --;
+
+    }
+    console.log("Reversed String: " + rev.toString());
   
 }
+
+reverseStr("Chrischale");
