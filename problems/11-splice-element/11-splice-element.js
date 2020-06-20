7ec2fa16af161d22d1501de3b7ef76d3e26b66dd
Chrischale Panditharathne <cpanda27@bu.edu>
2020-06-20 18:16:56 -0400

    chrish q11

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,10 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
+    return someArr.splice(index);
   
 }
+
+const arr = ['jenn', 'percy', 'mangnolia', 42, 76, true]
+console.log(spliceElement(arr, 4))
+//Note to self: "index" parameter implies the ith element in the array not the element at arr[index]
