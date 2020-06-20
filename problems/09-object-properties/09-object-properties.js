bf785bfc1f45769caa42c2fcce7fd71c0d61e79d
Chrischale Panditharathne <cpanda27@bu.edu>
2020-06-20 17:07:39 -0400

    chrish q9

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -2,5 +2,22 @@
 Define function objectProperties(someObj)
 Print every property and it's value. */
 function objectProperties(someObj) {
+    for(prop in someObj){
+        console.log(prop + ": " + someObj[prop]);
+
+    }
 
 }
+
+
+let ninja = {
+    name: 'Naruto',
+    age: 12,
+    power: 'wind',
+    warVet:true,
+    alive:true,
+    villains_defeated: 100
+
+}
+
+objectProperties(ninja);
