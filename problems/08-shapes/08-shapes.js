a603d73f784f912c89e243f2bbb5befea5524d36
Chrischale Panditharathne <cpanda27@bu.edu>
2020-06-20 13:43:30 -0400

    chrish q8

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -22,4 +22,86 @@ Example for printShape("Diamond", 5, "*");
 */
 function printShape(shape, height, character) {
   
+  switch(shape){
+    case "Square":
+      for (i = 0; i<height; i++){
+        let str = "";
+        for (j = 0; j < height; j++){
+          str += character;
+
+        }
+        console.log(str);
+      }
+      break;
+
+    case "Triangle":
+      for(i = 1; i <= height; i++){
+        let x = 1;
+        let str = "" ;
+          do{
+            str += character;
+            x++;
+          }while(x <= i)
+        console.log(str);
+      }
+      break;
+
+    case "Diamond":
+      //keep track of number of char to print in each row
+      let i = 1;
+      
+      //build first half of diamond
+      while(i <= height) {
+        //counter1 keeps track of how many spaces to print in each row
+        let c1 = (height - i)/2;
+        let str = "";
+
+        for(let a = 0; a < c1; a++){
+          str+= " "
+        }
+
+        for (let b = 0; b < i; b++){
+          str+= character;
+        }
+
+        console.log(str)
+
+        //increment by 2 to keep the number of chars odd i.e. 1, 3, 5, 7 ..
+        i += 2;
+        
+      }
+     
+      
+      //build second half of diamond
+      while(i >= 2) {
+        let str = "";
+        i = i - 2;
+        //counter 2 keeps track of spaces for second half
+        let c2 = (height - i)/2;
+        //don't print middle line twice if its an odd height
+        if(i < height) {                   
+          for(let a = 0; a < c2; a++){
+            str+= " "
+          }
+  
+          for (let b = 0; b < i; b++){
+            str+= character;
+          }
+
+          console.log(str)
+        } 
+      }
+      
+        
+
+    
+    
+    }
+  
 }
+
+
+printShape("Diamond", 7, "#");
+printShape("Diamond", 5, "#");
+printShape("Diamond", 10, "#");
+printShape("Diamond", 1, "#");
