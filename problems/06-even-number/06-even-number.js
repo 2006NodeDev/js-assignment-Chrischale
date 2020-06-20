df446f1bbfe477b684f8624badd77e40bb2b5bd1
Chrischale Panditharathne <cpanda27@bu.edu>
2020-06-20 18:46:22 -0400

    minor comment fixes

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -3,15 +3,30 @@ Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
 function isEven(someNum) {
+    //handle negative numbers
+    if (someNum < 0){
+        someNum = someNum * (-1)
+    }
+
     let i = 0;
     let flag = true;
-    while(i<someNum){
-        flag = !flag;
+
+    //set a flag that states if the value is even
+    //flip flag as counter i increments one by one
+    while(i < someNum){
         i++;
+        flag = !flag;
     }
 
     return flag;
 
 }
-let j = 5;
+
+let j = -5;
 console.log("Is " + j + " even? " + isEven(j));
+
+let k = 10;
+console.log("Is " + k + " even? " + isEven(k));
+
+let l = 2395894825;
+console.log("Is " + l + " even? " + isEven(l));
